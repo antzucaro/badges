dc16ba7facdeb175da7edcb812b7ddc3a42cc22f
Ant Zucaro <azucaro@gmail.com>
2015-09-26 16:12:54 -0400

    Initial work on Skins.

---
+++ skin.go
@@ -0,0 +1,54 @@
+package main
+
+// RGB is an RGB value
+type RGB struct {
+	Red   float64
+	Green float64
+	Blue  float64
+}
+
+// Position is an (x,y) coordinate
+type Position struct {
+	X int
+	Y int
+}
+
+// BasicTextConfig provides basic style information for Cairo text
+type BasicTextConfig struct {
+	FontSize  int
+	Pos       Position
+	Color     RGB
+	TopColor  RGB
+	MidColor  RGB
+	BotColor  RGB
+	Angle     int
+	TextFmt   string
+	UpperCase bool
+	Height    int
+	Width     int
+	MaxWidth  int
+}
+
+// SkinParams represents parameters given to Skin objects
+type SkinParams struct {
+	Background        string
+	BackgroundColor   RGB
+	Overlay           string
+	Font              string
+	Width             int
+	Height            int
+	NumGameTypes      int
+	NickConfig        BasicTextConfig
+	GameTypeConfig    BasicTextConfig
+	NoStatsConfig     BasicTextConfig
+	EloConfig         BasicTextConfig
+	RankConfig        BasicTextConfig
+	WinConfig         BasicTextConfig
+	WinPctConfig      BasicTextConfig
+	LossConfig        BasicTextConfig
+	KDConfig          BasicTextConfig
+	KDRatio           BasicTextConfig
+	KillsConfig       BasicTextConfig
+	DeathsConfig      BasicTextConfig
+	PlayingTimeConfig BasicTextConfig
+}
