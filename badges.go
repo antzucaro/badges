a71c246bba308617b2e84c4bc4953dc1ceae4fbf
Ant Zucaro <azucaro@gmail.com>
2017-03-11 07:21:49 -0500

    Fill in the single-pid option.

--- badges.go
+++ badges.go
@@ -26,6 +26,7 @@ func main() {
 	}
 
 	if *pid == -1 {
+        // Find players by delta, with an optional limit
 		pids, err := pp.FindPlayers(*delta, *limit)
 		if err != nil {
 			log.Fatal(err)
@@ -38,5 +39,12 @@ func main() {
 			}
 			fmt.Printf("%+v\n", *pd)
 		}
-	}
+	} else {
+        // Find a specific player by ID
+        pd, err := pp.GetPlayerData(*pid)
+        if err != nil {
+			log.Fatal(err)
+        }
+        fmt.Printf("%+v\n", *pd)
+    }
 }
