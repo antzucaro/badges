dd1cdfd17f7df42f779c065a31edc9df0b32f7df
Ant Zucaro <azucaro@gmail.com>
2017-03-11 18:44:02 -0500

    Change the name, remove references to Cairo.

--- skin.go
+++ skin.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"github.com/ungerik/go-cairo"
-	"math"
+	"github.com/fogleman/gg"
 )
 
 // RGB is an RGB value
@@ -18,21 +17,30 @@ type Position struct {
 	Y int
 }
 
-// BasicTextConfig provides basic style information for Cairo text
-type BasicTextConfig struct {
-	FontSize  int
-	Pos       Position
-	Color     RGB
-	TopColor  RGB
-	MidColor  RGB
-	BotColor  RGB
-	Angle     int
-	TextFmt   string
-	UpperCase bool
-	Height    int
-	Width     int
-	MaxWidth  int
-	Align     int
+// TextConfig provides information used to place text on a canvas
+type TextConfig struct {
+	// The full path to the TTF font file to be loaded
+	Font string
+
+	// The size in points the font will be
+	FontSize int
+
+	// The X and Y location where the bottom-left of the text will begin
+	Pos Position
+
+	// The color the text will be
+	Color RGB
+
+	// Rotation
+	Angle int
+
+	// Dimensions
+	Height   int
+	Width    int
+	MaxWidth int
+
+	// Text alignment: left: -1, center: 0, right: 1
+	Align int
 }
 
 // SkinParams represents parameters given to Skin objects
@@ -44,26 +52,26 @@ type SkinParams struct {
 	Width             int
 	Height            int
 	NumGameTypes      int
-	NickConfig        BasicTextConfig
-	GameTypeConfig    BasicTextConfig
-	NoStatsConfig     BasicTextConfig
-	EloConfig         BasicTextConfig
-	RankConfig        BasicTextConfig
-	WinConfig         BasicTextConfig
-	WinPctConfig      BasicTextConfig
-	LossConfig        BasicTextConfig
-	KDConfig          BasicTextConfig
-	KDRatio           BasicTextConfig
-	KillsConfig       BasicTextConfig
-	DeathsConfig      BasicTextConfig
-	PlayingTimeConfig BasicTextConfig
+	NickConfig        TextConfig
+	GameTypeConfig    TextConfig
+	NoStatsConfig     TextConfig
+	EloConfig         TextConfig
+	RankConfig        TextConfig
+	WinConfig         TextConfig
+	WinPctConfig      TextConfig
+	LossConfig        TextConfig
+	KDConfig          TextConfig
+	KDRatio           TextConfig
+	KillsConfig       TextConfig
+	DeathsConfig      TextConfig
+	PlayingTimeConfig TextConfig
 }
 
 // Skin represents the look and feel of a XonStat badge
 type Skin struct {
 	Name    string
 	Params  SkinParams
-	surface *cairo.Surface
+	Context *gg.Context
 }
 
 // String representation of a Skin
@@ -71,116 +79,100 @@ func (s *Skin) String() string {
 	return s.Name
 }
 
-// ShowText shows the given text on the surface with alignment and angling.
-func (s *Skin) ShowText(text string, pos Position, align int, angle int, offsetX int, offsetY int) {
-	te := s.surface.TextExtents(text)
-
-	if align > 0 {
-		s.surface.MoveTo(float64(pos.X+offsetX)-te.Xbearing, float64(pos.Y+offsetY)-te.Ybearing)
-	} else if align < 0 {
-		s.surface.MoveTo(float64(pos.X+offsetX)-te.Xbearing-te.Width, float64(pos.Y+offsetY)-te.Ybearing)
-	} else {
-		s.surface.MoveTo(float64(pos.X+offsetX)-te.Xbearing-te.Width/2, float64(pos.Y+offsetY)-te.Ybearing)
-	}
-	s.surface.Save()
-
-	if angle > 0 {
-		s.surface.Rotate(float64(angle) * math.Pi / 180.0)
-	}
-
-	s.surface.ShowText(text)
-	s.surface.Restore()
+// Render the provided PlayerData using this Skin
+func (s *Skin) Render(pd *PlayerData, output string) {
 }
 
 // The "classic" skin theme
-var DefaultSkin = Skin{
-	Name: "classic",
+var ArcherSkin = Skin{
+	Name: "archer",
 	Params: SkinParams{
-		Background:      "",
+		Background:      "images/background_archer-v3.png",
 		BackgroundColor: RGB{Red: 0.00, Green: 0.00, Blue: 0.00},
 		Overlay:         "",
 		Font:            "Xolonium",
 		Width:           560,
 		Height:          720,
 		NumGameTypes:    3,
-		NickConfig: BasicTextConfig{
+		NickConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 22,
 			Pos:      Position{X: 53, Y: 20},
 			MaxWidth: 270,
 		},
-		GameTypeConfig: BasicTextConfig{
+		GameTypeConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 10,
 			Pos:      Position{X: 101, Y: 33},
 			Color:    RGB{Red: 0.09, Green: 0.09, Blue: 0.09},
-			TextFmt:  "%s",
 			Width:    94,
 		},
-		NoStatsConfig: BasicTextConfig{
+		NoStatsConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 12,
 			Pos:      Position{X: 101, Y: 59},
 			Color:    RGB{Red: 0.8, Green: 0.2, Blue: 0.1},
 			Angle:    -10,
-			TextFmt:  "no stats yet!",
 		},
-		EloConfig: BasicTextConfig{
+		EloConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 10,
 			Pos:      Position{X: 101, Y: 47},
 			Color:    RGB{Red: 1.0, Green: 1.0, Blue: 0.5},
-			TextFmt:  "Elo %.0f",
 		},
-		RankConfig: BasicTextConfig{
+		RankConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 8,
 			Pos:      Position{X: 101, Y: 58},
 			Color:    RGB{Red: 0.8, Green: 0.8, Blue: 1.0},
-			TextFmt:  "Rank %d of %d",
 		},
-		WinConfig: BasicTextConfig{
-			FontSize:  10,
-			Pos:       Position{X: 508, Y: 3},
-			Color:     RGB{Red: 0.8, Green: 0.8, Blue: 0.8},
-			TextFmt:   "Win Percentage",
-			UpperCase: true,
+		WinConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
+			FontSize: 10,
+			Pos:      Position{X: 508, Y: 3},
+			Color:    RGB{Red: 0.8, Green: 0.8, Blue: 0.8},
 		},
-		WinPctConfig: BasicTextConfig{
+		WinPctConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 15,
 			Pos:      Position{X: 509, Y: 18},
 			Color:    RGB{Red: 0.00, Green: 0.00, Blue: 0.00},
-			TopColor: RGB{Red: 0.2, Green: 1.0, Blue: 1.0},
-			MidColor: RGB{Red: 0.4, Green: 0.8, Blue: 0.4},
-			BotColor: RGB{Red: 1.0, Green: 1.0, Blue: 0.2},
 		},
-		LossConfig: BasicTextConfig{
+		LossConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 9,
 			Pos:      Position{X: 508, Y: 44},
 			Color:    RGB{Red: 0.8, Green: 0.8, Blue: 0.6},
 		},
-		KDConfig: BasicTextConfig{
+		KDConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 10,
 			Pos:      Position{X: 390, Y: 3},
 			Color:    RGB{Red: 0.8, Green: 0.8, Blue: 0.8},
-			TextFmt:  "Kill Ratio",
 			Width:    102,
 		},
-		KDRatio: BasicTextConfig{
+		KDRatio: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 15,
 			Pos:      Position{X: 392, Y: 18},
 		},
-		KillsConfig: BasicTextConfig{
+		KillsConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 9,
 			Pos:      Position{X: 392, Y: 33},
 			Color:    RGB{Red: 0.6, Green: 0.8, Blue: 0.6},
 		},
-		DeathsConfig: BasicTextConfig{
+		DeathsConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 9,
 			Pos:      Position{X: 392, Y: 44},
 			Color:    RGB{Red: 0.8, Green: 0.6, Blue: 0.6},
 		},
-		PlayingTimeConfig: BasicTextConfig{
+		PlayingTimeConfig: TextConfig{
+			Font:     "fonts/xolonium.ttf",
 			FontSize: 10,
 			Pos:      Position{X: 451, Y: 59},
 			Color:    RGB{Red: 0.1, Green: 0.1, Blue: 0.1},
-			TextFmt:  "Playing Time %s",
 		},
 	},
-	surface: nil,
 }
