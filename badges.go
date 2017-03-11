eef61215092975a6c75b5a35aa2b39edaeea2d23
Ant Zucaro <azucaro@gmail.com>
2017-03-11 07:33:57 -0500

    Factor out the fetching of player data by list of pids.

--- badges.go
+++ badges.go
@@ -25,26 +25,23 @@ func main() {
 		*delta = -1
 	}
 
+	var pids []int
 	if *pid == -1 {
-        // Find players by delta, with an optional limit
-		pids, err := pp.FindPlayers(*delta, *limit)
+		// Find players by delta, with an optional limit
+		pids, err = pp.FindPlayers(*delta, *limit)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		for _, v := range pids {
-			pd, err := pp.GetPlayerData(v)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Printf("%+v\n", *pd)
-		}
 	} else {
-        // Find a specific player by ID
-        pd, err := pp.GetPlayerData(*pid)
-        if err != nil {
-			log.Fatal(err)
-        }
-        fmt.Printf("%+v\n", *pd)
-    }
+		// Use just the one player ID for fetching
+		pids = []int{*pid}
+	}
+
+	for _, v := range pids {
+		pd, err := pp.GetPlayerData(v)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%+v\n", *pd)
+	}
 }
